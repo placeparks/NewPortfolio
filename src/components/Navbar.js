2ffd0913cd22391dbadf8f463b948c3128626185
placeparks <86215469+placeparks@users.noreply.github.com>
2023-12-23 19:09:30 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -65,7 +65,7 @@ const Navbar = () => {
     <li>    <NavLink className="dropdown-item" to="https://grammar-check-baa58ce59eec.herokuapp.com/" target="_blank" rel="noreferrer" smooth={true} duration={1000}>
       Grammar Check
       </NavLink></li>
-      <li>    <NavLink className="dropdown-item" to="https://github.com/placeparks/Image-Generator.git/" target="_blank" rel="noreferrer" smooth={true} duration={1000}>
+      <li>    <NavLink className="dropdown-item" to="https://gitdescriptionbot-fc3daf77924a.herokuapp.com/" target="_blank" rel="noreferrer" smooth={true} duration={1000}>
       Github Bot
       </NavLink></li>
       <li>    <NavLink className="dropdown-item" to="https://airesume-generator-a20b961d3ccb.herokuapp.com/" target="_blank" rel="noreferrer" smooth={true} duration={1000}>
